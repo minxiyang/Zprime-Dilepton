f96a51838b137cc1449411a44fe2729595d9d772
[Minxi <[yhzz598@gmail.com]>
2021-05-20 18:21:55 -0400

    updated plot code

--- plotMass.py
+++ plotMass.py
@@ -17,7 +17,10 @@ import matplotlib as mpl
 import matplotlib.style
 import matplotlib.font_manager
 import uproot
+import coffea.hist
 #mpl.style.use('classic')
+
+
 class frame:
 
         def __init__(self, xtitle, xRange, ytitle, yRange, bins, category, color, year, islogx, islogy, isMC, isFill):
@@ -36,240 +39,147 @@ class frame:
                 self.year = year
 #cfg
 path_DY="/depot/cms/users/minxi/NanoAOD_study/Zprime-mumu/output/DY/*/*.parquet"
-path_data="/depot/cms/users/minxi/NanoAOD_study/Zprime-mumu/output/datav2/*/*.parquet"
+path_data="/depot/cms/users/minxi/NanoAOD_study/Zprime-mumu/output/data/*/*.parquet"
 path_save="/depot/cms/users/minxi/NanoAOD_study/Zprime-mumu/plots/"
 path_signal="/depot/cms/users/minxi/NanoAOD_study/dileptonmassplots/inputs/paperPlotInputs_DimuonMass_Run2.root"
 variables_plot = 'dimuon_mass'
 frames={}
-#bins = [j for j in range(50, 120, 5)] + [j for j in range(120, 150, 5)] + [j for j in range(150, 200, 10)] + [j for j in range(200, 600, 20)] + [j for j in range(600, 900, 30) ] + [j for j in range(900, 1250, 50)] + [j for j in range(1250, 1610, 60) ] + [j for j in range(1610, 1890, 70) ] + [j for j in range(1890, 3970, 80) ] + [j for j in range(3970, 6070, 100) ] + [6070]
 
 bins = [120., 129.95474058, 140.73528833, 152.41014904, 165.0535115, 178.74571891, 193.57377942, 209.63191906,  227.02218049,  245.85507143,  266.2502669,   288.3373697,  312.25673399,  338.16035716,  366.21284574,  396.59246138,  429.49225362, 465.12128666,  503.70596789,  545.49148654,  590.74337185,  639.74918031, 692.82032303,  750.29404456,  812.53556599,  879.94040575,  952.93689296,  1031.98888927, 1117.59873655, 1210.310449,   1310.71317017, 1419.4449167,   1537.19663264, 1664.71658012, 1802.81509423, 1952.36973236, 2114.3308507, 2289.72764334, 2479.6746824,  2685.37900061, 2908.14776151, 3149.39656595, 3410.65844758, 3693.59361467, 4000.,4500,5200,6000,7000,8000]
 
 
 frames['dimuon_mass'] = frame('$\mathrm{m}(\mu^{+}\mu^{-})$ [GeV]', [120, 6000], 'Events/GeV', [1e-5, 1e7], np.array(bins), "Combine", "skyblue", "2018", True, True, True, True)
-#frames['dimuon_cos_theta_cs'] = frame('cos $\\theta$', [-1, 1], 'Events', [0, 50000], np.linspace(-1,1,25), "Combine", "cyan", "2018", False, False, True, True)
 lumis={'2016':36.3, '2017':42.1 ,'2018':61.6}
-np.linspace(-1, 1, 25)
-# Zprime->ll mass plot style
-Zprime2ll_mass = {
-    "font.sans-serif": ["TeX Gyre Heros", "Helvetica", "Arial"],
-    "font.family": "sans-serif",
-    "mathtext.fontset": "cm",
-#    "mathtext.rm": "TeX Gyre Heros",
-#    "mathtext.bf": "TeX Gyre Heros:bold",
-#    "mathtext.sf": "TeX Gyre Heros",
-#    "mathtext.it": "TeX Gyre Heros:italic",
-#    "mathtext.tt": "TeX Gyre Heros",
-#    "mathtext.cal": "DejaVu Serif:italic",
-#    "mathtext.default": "regular",
-#    "mathtext.fontset": "custom",
-#    "mathtext.rm": "stix",
-#    "mathtext.bf": "stix:bold",
-#    "mathtext.sf": "stix",
-#    "mathtext.it": "stix:italic",
-#    "mathtext.tt": "stix",
-#    "mathtext.cal": "stix:italic",
-#    "mathtext.default": "regular",
 
-    "figure.figsize": (10.0, 10.0),
-    "font.size": 26,
-    "axes.labelsize": "medium",
-    "axes.unicode_minus": False,
-    "xtick.labelsize": "small",
-    "ytick.labelsize": "small",
-    "legend.fontsize": "small",
-    "legend.handlelength": 1.5,
-    "legend.borderpad": 0.5,
-    "legend.frameon": False,
-    "xtick.direction": "in",
-    "xtick.major.size": 12,
-    "xtick.minor.size": 6,
-    "xtick.major.pad": 6,
-    "xtick.top": True,
-    "xtick.major.top": True,
-    "xtick.major.bottom": True,
-    "xtick.minor.top": True,
-    "xtick.minor.bottom": True,
-    "xtick.minor.visible": True,
-    "ytick.direction": "in",
-    "ytick.major.size": 12,
-    "ytick.minor.size": 6.0,
-    "ytick.right": True,
-    "ytick.major.left": True,
-    "ytick.major.right": True,
-    "ytick.minor.left": True,
-    "ytick.minor.right": True,
-    "ytick.minor.visible": True,
-    "grid.alpha": 0.8,
-    "grid.linestyle": ":",
-    "axes.linewidth": 2,
-    "savefig.transparent": False,
-    "xaxis.labellocation": "right",
-    "yaxis.labellocation": "top",}
-#    "text.usetex": True,
-#    "text.latex.preamble": r"\usepackage{siunitx},\sisetup{detect-all}, \
-#                              \usepackage{helvet},\usepackage{sansmath}, \
-#                              \sansmath",
+#Zprime->ll mass plot style
+style = hep.style.CMS
+style['mathtext.fontset'] = 'cm'
+style['mathtext.default'] = 'rm'
+plt.style.use(style)
+hep.set_style(style)
 
-#}
-
-# Filter extra (labellocation) items if needed
-Zprime2ll_mass = {k: v for k, v in Zprime2ll_mass.items() if k in mpl.rcParams}
-
-#rc('text', usetex=True)
-#plt.rcParams['text.usetex'] = True
-#plt.rcParams['text.latex.unicode'] = True
-#plt.rcParams['text.latex.preamble'] = r"\usepackage{siunitx},\sisetup{detect-all}, \
-#                              \usepackage{helvet},\usepackage{sansmath}, \
-#                              \sansmath"
-#import subprocess
-#print(subprocess.check_call(["latex"]))
-
-
-
-
-
-
-
-
-
-
-#hep.set_style(hep.style.CMS)
-#hep.set_style('CMSTex')
-#print(hep.style.CMSTex)
-#plt.style.use([hep.style.CMS])
-#hep.cms.label()
-#hep.cms.text(hep.style.CMSTex)
-hep.set_style([Zprime2ll_mass])
-plt.style.use([Zprime2ll_mass])
 # def function
 
 def plot(MC, data, Zprime, G_RS, variable, path_save):
         fig, axs = plt.subplots(2, sharex=True, sharey=False, gridspec_kw={'height_ratios': [4, 1], })
         plt.subplots_adjust(hspace=0.07)
-        #fig.tight_layout()
         frame = frames[variable]
         axs[1].set_xlabel(frame.xtitle)
         axs[0].set_ylabel(frame.ytitle)
-        #axs[0].yaxis.grid(True, which='minor')
         axs[1].set_ylabel("(Data-Bkg)/Bkg", fontsize=16)
         axs[1].set_xlim(frame.xRange)
         axs[0].set_ylim(frame.yRange)
         axs[1].set_ylim([-1,1])
-        #hep.cms.text("test", ax=axs[0]) 
+        axs[1].set_xticks([200, 300, 1000, 2000])
+        axs[1].get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
+        locmaj = LogLocator(base=10.0, subs=(1.0,), numticks=100)                           
+        locmin = LogLocator(base=10.0, subs=np.arange(2,10)*0.1, numticks=100)
+        axs[1].hlines((-0.5, 0, 0.5), frame.xRange[0], frame.xRange[1], color="black", linestyles="dotted")
+        ax_signal = axs[0].twiny()
+        ax_signal.set_xscale('log')
+        ax_signal.set_yscale('log')
+        ax_signal.set_ylim(frame.yRange)
+        ax_signal.set_xlim(frame.xRange)
+        ax_signal.set_yticks([1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6])
+        ax_signal.set_xticks([])
+        locmaj = LogLocator(base=10.0, subs=(1.0,), numticks=100)
+        ax_signal.get_yaxis().set_major_locator(locmaj)
+        ax_signal.yaxis.set_minor_locator(locmin)
+        ax_signal.yaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
+        
         if frame.islogx:
                 axs[0].set_xscale('log')
         if frame.islogy:
                 axs[0].set_yscale('log')
-        print(variable)
-        htype="step"
+
+
+
+
+
         MC_mass = MC[variable].compute()
         data_mass = data[variable].compute()
-        data_run = data['run'].compute()
-        m=data_mass[data_mass>2000].values
-        run=data_run[data_mass>2000].values
-        s = np.argsort(m)
-        s = s[::-1]
-        for i in s:
-            print("run %i with mass %f"%(run[i], m[i]))
+
+        #data_run = data['run'].compute()
+        #m=data_mass[data_mass>2000].values
+        #run=data_run[data_mass>2000].values
+        #s = np.argsort(m)
+        #s = s[::-1]
+        #for i in s:
+        #    print("run %i with mass %f"%(run[i], m[i]))
+
+
         MC_entries = MC_mass.values
         data_entries = data_mass.values
         weight=(MC['wgt_nominal'].compute()).values
-        MC_yvals, bins  = np.histogram(MC_entries, bins=frame.bins, weights=weight)
-        data_yvals, bins  = np.histogram(data_entries, bins=frame.bins)
-        if variable == "dimuon_mass":
-                binSize = np.diff(bins)
-                MC_yvals = MC_yvals/binSize
-                data_yvals = data_yvals/binSize
-                axs[1].set_xticks([200, 300, 1000, 2000])
-                #axs[0].set_yticks([1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6])
-                axs[1].get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
-                locmaj = LogLocator(base=10.0, subs=(1.0,), numticks=100)                   
-                #axs[0].get_yaxis().set_major_locator(locmaj)
-                locmin = LogLocator(base=10.0, subs=np.arange(2,10)*0.1, numticks=100)
-                #axs[0].yaxis.set_minor_locator(locmin)
-                #axs[0].yaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
-                axs[1].hlines((-0.5, 0, 0.5), frame.xRange[0], frame.xRange[1], color="black", linestyles="dotted")
-                ax_signal = axs[0].twiny()
-                ax_signal.set_xscale('log')
-                ax_signal.set_yscale('log')
-                ax_signal.set_ylim(frame.yRange)
-                ax_signal.set_xlim(frame.xRange)
-                ax_signal.set_yticks([1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6])
-                locmaj = LogLocator(base=10.0, subs=(1.0,), numticks=100)
-                ax_signal.get_yaxis().set_major_locator(locmaj)
-                ax_signal.yaxis.set_minor_locator(locmin)
-                ax_signal.yaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
-                ax_signal.set_xticks([])
+        MC_vals, bins  = np.histogram(MC_entries, bins=frame.bins, weights=weight)
+        MC_vals2, bins  = np.histogram(MC_entries, bins=frame.bins, weights=weight**2)
+        data_vals, bins  = np.histogram(data_entries, bins=frame.bins)
+        MC_errs = np.sqrt(MC_vals2)
+        data_errs = np.sqrt(data_vals)
+        binSize = np.diff(bins)
+        MC_vals = MC_vals/binSize
+        data_vals = data_vals/binSize
+        MC_errs = MC_errs/binSize
+        data_errs = data_errs/binSize
+        
+        r_vals = data_vals/MC_vals
+        r_errs = r_vals*np.sqrt((data_errs/data_vals)**2+(MC_errs/MC_vals)**2)
+        r_MCerrs = MC_errs/MC_vals
 
-                #axs[1].hlines(y=0.)
-                #axs[1].hlines(y=0.5)
-                #axs[0].tick_params(which='both', width=2)
-                #axs[0].tick_params(which='major', length=7)
-                #axs[0].tick_params(which='minor', length=4)
+        #MC_errR = coffea.hist.poisson_interval(MC_vals, MC_vals2)
+        #print(MC_errR)
+        #lower = np.array(MC_errR[0])
+        #upper = np.array(MC_errR[1])
+        #MC_errs = (lower+upper)/2.
 
-                #axs[0].get_yaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
-                #axs[0].ticklabel_format(axis='y',style='sci',scilimits=(0,0))
-
-        if frame.isFill:
-                htype="fill"
-
-        #hep.histplot([data_yvals, MC_yvals], bins, ax=axs[0], color=['black', frame.color], histtype=['errorbar' ,htype],label=["Data" ,"$\gamma$ /Z$\\rightarrow \mu^{+}\mu^{-}$"])
-        #hep.histplot( MC_yvals, bins, ax=axs[0], color=frame.color, histtype=htype, label="$\gamma$ /Z$\\rightarrow \mu^{+}\mu^{-}$")
+        
         lumi=lumis[frame.year]
         Zprime = Zprime*lumi/140.
         G_RS = G_RS*lumi/140.
-        hep.histplot(data_yvals, bins, ax=axs[0], color='black', histtype='errorbar',label="Data", yerr=True)
-        hep.histplot(MC_yvals, bins, ax=axs[0], color=frame.color, histtype=htype, label="$\gamma/\mathrm{Z}\\rightarrow \mu^{+}\mu^{-}$", edgecolor=(0,0,0))
-        #ax_signal.set_yticks([])
+        
+        hep.histplot(data_vals, bins, ax=axs[0], color='black', histtype='errorbar',label="Data", yerr=data_errs)
+        hep.histplot(MC_vals, bins, ax=axs[0], color=frame.color, histtype='fill', label="$\gamma/\mathrm{Z}\\rightarrow \mu^{+}\mu^{-}$", edgecolor=(0,0,0))
+        bins_mid = (bins[1:]+bins[:-1])/2
+        axs[0].fill_between(x=bins_mid, y1=MC_vals-MC_errs, y2=MC_vals+MC_errs, interpolate=True, color='skyblue', alpha=0.3) 
         hep.histplot(G_RS, bins, ax=ax_signal, color='green', histtype='step', label='$G_{RS}, k/\\bar{M}_{Pl}$ = 0.01, M = 2 TeV')
         hep.histplot(Zprime, bins, ax=ax_signal, color='magenta', histtype='step', label="$Z'_{SSM}$, M = 5 TeV", linestyle='dashed')
-        rvals = (data_yvals-MC_yvals)/MC_yvals
         axs[0].legend(loc=(0.35,0.7))
         ax_signal.legend(loc=(0.45,0.55),fontsize='xx-small')
-        hep.histplot(rvals, bins, ax=axs[1], histtype='errorbar', color='black', yerr=True)
-        #lumi=lumis[frame.year]
-        #Zprime = Zprime*lumi/140.
-        #G_zy = G_zy*lumi/140.
-        #print(Zprime)
-        #print(G_zy)
-        _lumi = r"{lumi} (13 TeV)".format(lumi=str(lumi) + r" fb$^{-1}$")
-        print(_lumi)
-        axs[0].text(1.0, 1.08, _lumi, verticalalignment='top', horizontalalignment='right', transform=axs[0].transAxes, fontsize=28)
-        axs[0].text(0.88, 0.95, "CMS", verticalalignment='top', horizontalalignment='right', transform=axs[0].transAxes, fontsize=37, weight='bold')
-        #hep.CMS.label(lumi=lumi, exp="")
-        
-        #hep.cms.text()
-        fig.savefig(path_save+variable+"_nocms.pdf")
 
+        hep.histplot(r_vals-1, bins, ax=axs[1], histtype='errorbar', color='black', yerr=r_errs)
+        axs[1].fill_between(x=bins_mid, y1=-r_MCerrs, y2=r_MCerrs, interpolate=True, color='skyblue', alpha=0.3)
+        
+        _lumi = r"{lumi} (13 TeV)".format(lumi=str(lumi) + r" fb$^{-1}$")
+        axs[0].text(1.0, 1.08, _lumi, verticalalignment='top', horizontalalignment='right', transform=axs[0].transAxes, fontsize=28)
+        axs[0].text(0.1, 0.92, 'SR', verticalalignment='top', horizontalalignment='left', transform=axs[0].transAxes, fontsize=28)
+        axs[0].text(0.88, 0.95, "CMS", verticalalignment='top', horizontalalignment='right', transform=axs[0].transAxes, fontsize=37, weight='bold')
+        
+        fig.savefig(path_save+variable+".pdf")
+
+
+
+if __name__=="__main__":
 
 # load signal
 
-Zprime = uproot.open(path_signal)['signalHist_0'].values()
-Zprime = np.array(Zprime[-len(bins)+1:])
+    Zprime = uproot.open(path_signal)['signalHist_0'].values()
+    Zprime = np.array(Zprime[-len(bins)+1:])
 
-G_RS = uproot.open(path_signal)['signalHist_1'].values()
-G_RS = np.array(G_RS[-len(bins)+1:])
+    G_RS = uproot.open(path_signal)['signalHist_1'].values() 
+    G_RS = np.array(G_RS[-len(bins)+1:])
 
-#print(Zprime)
-#print(G_zy)
 #load data 
-DY_paths = glob.glob(path_DY)
-DY_paths = [p for p in DY_paths if '0' in p]
-with ProcessPoolExecutor(max_workers=48) as executor:
+    DY_paths = glob.glob(path_DY)
+    with ProcessPoolExecutor(max_workers=48) as executor:
         DY_dfs = list(executor.map(dd.read_parquet, DY_paths))
-DY_df=dd.concat(DY_dfs)
+    DY_df=dd.concat(DY_dfs)
 
-data_paths = glob.glob(path_data)
-data_paths = [p for p in data_paths if '0' in p]
-with ProcessPoolExecutor(max_workers=48) as executor:
+    data_paths = glob.glob(path_data)
+    with ProcessPoolExecutor(max_workers=48) as executor:
         data_dfs = list(executor.map(dd.read_parquet, data_paths))
-data_df=dd.concat(data_dfs)
-#print(data.head())
-#plot
-#with ProcessPoolExecutor(max_workers=48) as executor:
-#	executor.map(plot, itertools.repeat(DY_df),  ,variables_plot, itertools.repeat(path_save))
-plot(DY_df, data_df, Zprime, G_RS, variables_plot, path_save)
+    data_df=dd.concat(data_dfs)
+
+    plot(DY_df, data_df, Zprime, G_RS, variables_plot, path_save)
 
 
 
