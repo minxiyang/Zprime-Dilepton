b71e1da7cc56236230cba96bc1e384742d0a2344
Jan-Frederik Schulte <jschulte@cern.ch>
2022-05-16 09:36:12 -0400

    first try at fixing flake8 complains for muon reco weights

--- processNano/corrections/muonMassScale.py
+++ processNano/corrections/muonMassScale.py
@@ -6,20 +6,20 @@ def calcMassScaleUncert(mass, isBB, year, up=True):
     scaleUnc = 0.0
 
     if (year == 2016):
-        if (isBB): 
+        if (isBB):
             scaleUnc = -0.01
-        else: 
+        else:
             scaleUnc = -0.03
     elif (year == 2017):
-        if (isBB): 
-            scaleUnc = 1. - (1.00037 -1.70128e-06*mass + 3.09916e-09*mass**2 -1.32999e-12*mass**3 +  2.99434e-16*mass**4 + -2.28065e-20*mass**5)
-        else: 
-            scaleUnc = 1. - (1.00263 - 1.04029e-05*mass + 8.9214e-09*mass**2 -3.4176e-12*mass**3 +  6.07934e-16*mass**4  -3.73738e-20*mass**5)
+        if (isBB):
+            scaleUnc = 1. - (1.00037 - 1.70128e-06*mass + 3.09916e-09*mass**2 - 1.32999e-12*mass**3 + 2.99434e-16*mass**4 - 2.28065e-20*mass**5)
+        else:
+            scaleUnc = 1. - (1.00263 - 1.04029e-05*mass + 8.9214e-09*mass**2 - 3.4176e-12*mass**3 + 6.07934e-16*mass**4 - 3.73738e-20*mass**5)
     elif (year == 2018):
-        if (isBB): 
-            scaleUnc = 1. - (0.999032 + 3.36979e-06*mass -3.4122e-09*mass**2 + 1.62541e-12*mass**3  - 3.12864e-16*mass**4 + 2.18417e-20*mass**5)
-        else: 
-            scaleUnc = 1. - (1.00051 - 2.21167e-06*mass + 2.21347e-09*mass**2 -7.72178e-13*mass**3 +  1.28101e-16*mass**4  - 8.32675e-21*mass**5)
+        if (isBB):
+            scaleUnc = 1. - (0.999032 + 3.36979e-06*mass - 3.4122e-09*mass**2 + 1.62541e-12*mass**3 - 3.12864e-16*mass**4 + 2.18417e-20*mass**5)
+        else:
+            scaleUnc = 1. - (1.00051 - 2.21167e-06*mass + 2.21347e-09*mass**2 - 7.72178e-13*mass**3 + 1.28101e-16*mass**4  - 8.32675e-21*mass**5)
 
     if up:
         scaleUnc = 1 - scaleUnc
--- processNano/corrections/muonRecoUncert.py
+++ processNano/corrections/muonRecoUncert.py
@@ -1,37 +1,45 @@
 from numpy import exp, sin, arctan
 import numpy as np
 
-def calcMuonRecoUncert(mass, pT1, pT2, eta1, eta2,isDimuon,year):
 
-    if not isDimuon: return 1.0
+def calcMuonRecoUncert(mass, pT1, pT2, eta1, eta2, isDimuon, year):
 
-    p1 = pT1 / (sin(arctan(2*exp(-1*eta1)))) 
-    p2 = pT2 / (sin(arctan(2*exp(-1*eta2)))) 
+    if not isDimuon:
+        return 1.0
+
+    p1 = pT1 / (sin(arctan(2*exp(-1*eta1))))
+    p2 = pT2 / (sin(arctan(2*exp(-1*eta2))))
 
     bb = True
-    if (abs(eta1) > 1.2 or abs(eta2) > 1.2): bb = False
+    if (abs(eta1) > 1.2 or abs(eta2) > 1.2): 
+        bb = False
 
     if year == 2016:
         SF1 = 1.0
         SF2 = 1.0
-        if (abs(eta1) <= 1.6 and p1 > 100): SF1 = (0.994 - 4.08e-6 * p1)/(0.994 - 4.08e-6 * 100)
-        elif (abs(eta1) > 1.6 and p1 > 200): SF1 = ((0.9784 - 4.73e-5 * p1)/(0.9908 - 1.26e-5 * p1)) / ((0.9784 - 4.73e-5 * 200)/(0.9908 - 1.26e-5 * 200))
-        if (abs(eta2) <= 1.6 and p2 > 100): SF2 = (0.994 - 4.08e-6 * p2)/(0.994 - 4.08e-6 * 100)
-        elif (abs(eta2) > 1.6 and p2 > 200): SF2 = ((0.9784 - 4.73e-5 * p2)/(0.9908 - 1.26e-5 * p2)) / ((0.9784 - 4.73e-5 * 200)/(0.9908 - 1.26e-5 * 200) )
+        if (abs(eta1) <= 1.6 and p1 > 100):
+            SF1 = (0.994 - 4.08e-6 * p1)/(0.994 - 4.08e-6 * 100)
+        elif (abs(eta1) > 1.6 and p1 > 200):
+            SF1 = ((0.9784 - 4.73e-5 * p1)/(0.9908 - 1.26e-5 * p1)) / ((0.9784 - 4.73e-5 * 200)/(0.9908 - 1.26e-5 * 200))
+        if (abs(eta2) <= 1.6 and p2 > 100):
+            SF2 = (0.994 - 4.08e-6 * p2)/(0.994 - 4.08e-6 * 100)
+        elif (abs(eta2) > 1.6 and p2 > 200):
+            SF2 = ((0.9784 - 4.73e-5 * p2)/(0.9908 - 1.26e-5 * p2)) / ((0.9784 - 4.73e-5 * 200)/(0.9908 - 1.26e-5 * 200))
 
         return SF1*SF2
 
     elif year == 2017:
 
-        if bb: return 0.99;
+        if bb:
+            return 0.99
 
         else:
             eff_default = 0
             eff_syst = 0
 
             if (mass <= 450):
-                a =  13.39
-                b =  6.696
+                a = 13.39
+                b = 6.696
                 c = -4.855e+06
                 d = -7.431e+06
                 e = -108.8
@@ -39,16 +47,16 @@ def calcMuonRecoUncert(mass, pT1, pT2, eta1, eta2,isDimuon,year):
                 eff_default = a - b * exp( -( (mass - c) / d) ) + e * mass**f
 			
             else:
-                eff_a     =  0.3148
-                eff_b     =  0.04447
-                eff_c     =  1.42
-                eff_d     = -5108.
-                eff_e     =  713.5
-                eff_default = eff_a + eff_b * mass**eff_c * exp(- ((mass - eff_d ) / eff_e) )
+                eff_a = 0.3148
+                eff_b = 0.04447
+                eff_c = 1.42
+                eff_d = -5108.
+                eff_e = 713.5
+                eff_default = eff_a + eff_b * mass**eff_c * exp(- ((mass - eff_d ) / eff_e))
 
             if (mass <= 450):
-                a =  1.33901e+01
-                b =  6.69687e+00
+                a = 1.33901e+01
+                b = 6.69687e+00
                 c = -4.85589e+06
                 d = -7.43036e+06
                 e = -1.14263e+02
@@ -56,63 +64,64 @@ def calcMuonRecoUncert(mass, pT1, pT2, eta1, eta2,isDimuon,year):
                 eff_syst= a - b * exp( -( (mass - c) / d) ) + e * mass**f
 			
             else:
-                eff_a     =  3.07958e-01
-                eff_b     =  4.63280e-02
-                eff_c     =  1.35632e+00
-                eff_d     = -5.00475e+03
-                eff_e     =  7.38088e+02
-                eff_syst =  eff_a + eff_b * mass**eff_c * exp(- ((mass - eff_d ) / eff_e) )
+                eff_a = 3.07958e-01
+                eff_b = 4.63280e-02
+                eff_c = 1.35632e+00
+                eff_d = -5.00475e+03
+                eff_e = 7.38088e+02
+                eff_syst =  eff_a + eff_b * mass**eff_c * exp(- ((mass - eff_d ) / eff_e))
 
             return eff_syst/eff_default
 
 
     elif year == 2018:
 
-        if bb: return 0.99;
+        if bb:
+            return 0.99
 
         else:
             eff_default = 0
             eff_syst = 0
 
             if (mass <= 450):
-                a =  13.39
-                b =  6.696;
-                c = -4.855e+06;
-                d = -7.431e+06;
-                e = -108.8;
-                f = -1.138;
+                a = 13.39
+                b = 6.696
+                c = -4.855e+06
+                d = -7.431e+06
+                e = -108.8
+                f = -1.138
                 eff_default = a - b * exp( -( (mass - c) / d) ) + e * mass**f
             else:
-                eff_a     =  0.3148
-                eff_b     =  0.04447
-                eff_c     =  1.42
-                eff_d     = -5108.
-                eff_e     =  713.5
-                eff_default = eff_a + eff_b * mass**eff_c * exp(- ((mass - eff_d ) / eff_e) )
+                eff_a = 0.3148
+                eff_b = 0.04447
+                eff_c = 1.42
+                eff_d = -5108.
+                eff_e = 713.5
+                eff_default = eff_a + eff_b * mass**eff_c * exp(- ((mass - eff_d ) / eff_e))
 
             if (mass <= 450):
-                a =  1.33901e+01
-                b =  6.69687e+00
+                a = 1.33901e+01
+                b = 6.69687e+00
                 c = -4.85589e+06
                 d = -7.43036e+06
                 e = -1.14263e+02
                 f = -1.15028e+00
                 eff_syst = a - b * exp( -( (mass - c) / d) ) + e * mass**f
             else:
-                eff_a     =  3.07958e-01
-                eff_b     =  4.63280e-02
-                eff_c     =  1.35632e+00
-                eff_d     = -5.00475e+03
-                eff_e     =  7.38088e+02
-                eff_syst =  eff_a + eff_b * mass**eff_c * exp(- ((mass - eff_d ) / eff_e) )
+                eff_a = 3.07958e-01
+                eff_b = 4.63280e-02
+                eff_c = 1.35632e+00
+                eff_d = -5.00475e+03
+                eff_e = 7.38088e+02
+                eff_syst =  eff_a + eff_b * mass**eff_c * exp(- ((mass - eff_d ) / eff_e))
 
-	
             return eff_syst/eff_default;
 
-def muonRecoUncert(masses, pT1, pT2, eta1, eta2,isDimuon,year):
+
+def muonRecoUncert(masses, pT1, pT2, eta1, eta2, isDimuon, year):
 
     weights = []
-    for i in range(0,masses.size):
-        weights.append(calcMuonRecoUncert(masses[i], pT1[i], pT2[i], eta1[i], eta2[i],isDimuon[i],int(year))) 
+    for i in range(0, masses.size):
+        weights.append(calcMuonRecoUncert(masses[i], pT1[i], pT2[i], eta1[i], eta2[i], isDimuon[i], int(year))) 
 
     return np.array(weights)
