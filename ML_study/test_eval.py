422a65ec308202c90b7da19faccae4164d354ae6
minxiyang <[yhzz598@gmail.com]>
2022-08-08 18:48:09 -0400

    update

--- ML_study/test_eval.py
+++ ML_study/test_eval.py
@@ -72,22 +72,22 @@ model.eval()
 
 sig_path = "output/trainData_v1/2018/sig/bbll_4TeV_*_posLL/*parquet"
 bkg_path = "output/trainData_v1/2018/bkg/*/*parquet"
-data_path = "output/trainData_v1/2018/data/*/*parquet"
+#data_path = "output/trainData_v1/2018/data/*/*parquet"
 sig_files = glob.glob(sig_path)
 bkg_files = glob.glob(bkg_path)
-data_files = glob.glob(data_path)
+#data_files = glob.glob(data_path)
 df_sig = dd.read_parquet(sig_files)
 df_sig = df_sig.compute()
 df_bkg = dd.read_parquet(bkg_files)
 df_bkg = df_bkg.compute()
-df_data = dd.read_parquet(data_files)
-df_data = df_data.compute()
+#df_data = dd.read_parquet(data_files)
+#df_data = df_data.compute()
 df_sig = df_sig[load_features]
 df_bkg = df_bkg[load_features]
-df_data = df_data[load_features]
+#df_data = df_data[load_features]
 df_sig = df_sig.loc[(abs(df_sig["jet1_eta"])<2.4) & (abs(df_sig["jet2_eta"])<2.4), :]
 df_bkg = df_bkg.loc[(abs(df_bkg["jet1_eta"])<2.4) & (abs(df_bkg["jet2_eta"])<2.4), :]
-df_data = df_data.loc[(abs(df_data["jet1_eta"])<2.4) & (abs(df_data["jet2_eta"])<2.4), :]
+#df_data = df_data.loc[(abs(df_data["jet1_eta"])<2.4) & (abs(df_data["jet2_eta"])<2.4), :]
 bkg_yield = sum(df_bkg.wgt_nominal)
 sig_yield = sum(df_sig.wgt_nominal)
 df_sig["wgt_nominal"] = df_sig["wgt_nominal"]*(bkg_yield/sig_yield)
@@ -96,8 +96,8 @@ sig = df_sig.drop(columns = ["wgt_nominal", "dataset", "dimuon_mass"]).values
 sig_wgt = df_sig["wgt_nominal"].values
 bkg = df_bkg.drop(columns = ["wgt_nominal", "dataset", "dimuon_mass"]).values
 bkg_wgt = df_bkg["wgt_nominal"].values
-data = df_data.drop(columns = ["wgt_nominal", "dataset", "dimuon_mass"]).values
-data_wgt = df_data["wgt_nominal"].values
+#data = df_data.drop(columns = ["wgt_nominal", "dataset", "dimuon_mass"]).values
+#data_wgt = df_data["wgt_nominal"].values
  
 sig = torch.from_numpy(sig).to(device)
 sig_scores = model(sig.double()) 
@@ -109,15 +109,15 @@ bkg_scores = model(bkg.double())
 bkg_scores = bkg_scores.cpu().detach().numpy()
 bkg_scores = bkg_scores.ravel()
 
-data = torch.from_numpy(data).to(device)
-data_scores = model(data.double())
-data_scores = data_scores.cpu().detach().numpy()
-data_scores = data_scores.ravel()
+#data = torch.from_numpy(data).to(device)
+#data_scores = model(data.double())
+#data_scores = data_scores.cpu().detach().numpy()
+#data_scores = data_scores.ravel()
 
 bins = np.linspace(0, 1, 100)
 plt.hist(sig_scores, bins, weights=sig_wgt, alpha=0.3, label='sig')
 plt.hist(bkg_scores, bins, weights=bkg_wgt, alpha=0.3, label='bkg')
-plt.hist(data_scores, bins, weights=data_wgt, alpha=0.3, label='data')
+#plt.hist(data_scores, bins, weights=data_wgt, alpha=0.3, label='data')
 plt.legend(loc='upper right')
 plt.savefig("pic/bbll_4TeV_POSLL_scores.png")
 plt.clf()
@@ -126,21 +126,21 @@ for key in df_sig.drop(columns = ["wgt_nominal", "dataset"]).columns.to_list():
 
     fea_sig = df_sig[key].values
     fea_bkg = df_bkg[key].values
-    fea_data = df_data[key].values
-    max_ = np.max(fea_data)
-    min_ = np.min(fea_data)
+    #fea_data = df_data[key].values
+    max_ = np.max(fea_bkg)
+    min_ = np.min(fea_bkg)
     bins = np.linspace(min_, max_, 100)
 
     plt.hist(fea_sig, bins, weights=sig_wgt, alpha=0.3, label='sig')
     plt.hist(fea_bkg, bins, weights=bkg_wgt, alpha=0.3, label='bkg')
-    plt.hist(fea_data, bins, weights=data_wgt, alpha=0.3, label='data')
+    #plt.hist(fea_data, bins, weights=data_wgt, alpha=0.3, label='data')
     plt.legend(loc='upper right')
     plt.savefig(f"pic/bbll_4TeV_POSLL_{key}.png")
     plt.clf()
 
     plt.hist(fea_sig[sig_scores<0.2], bins, weights=sig_wgt[sig_scores<0.2], alpha=0.3, label='sig')
     plt.hist(fea_bkg[bkg_scores<0.2], bins, weights=bkg_wgt[bkg_scores<0.2], alpha=0.3, label='bkg')
-    plt.hist(fea_data[data_scores<0.2], bins, weights=data_wgt[data_scores<0.2], alpha=0.3, label='data')
+    #plt.hist(fea_data[data_scores<0.2], bins, weights=data_wgt[data_scores<0.2], alpha=0.3, label='data')
     plt.legend(loc='upper right')
     plt.savefig(f"pic/bbll_4TeV_POSLL_{key}_cut.png")
     plt.clf()
--- ML_study/test_train.py
+++ ML_study/test_train.py
@@ -31,6 +31,7 @@ load_features = [
     "jet2_mass_log",
     "njets",
     "met_log",
+    "dimuon_mass",
     "wgt_nominal",
     "dataset",
 ]
@@ -60,11 +61,11 @@ class NeuralNet(nn.Module):
 
 
 
-input_size = len(load_features) - 2
+input_size = len(load_features) - 3
 hidden_sizes = [128, 64, 32, 16, 16, 16, 16, 16, 16]
 num_classes = 1
 num_epochs = 20
-batch_size = 100
+batch_size = 128
 learning_rate = 0.0001
 
 model = NeuralNet(input_size, hidden_sizes, num_classes).to(device)
@@ -98,12 +99,14 @@ dataset = dataset.sample(frac=1.)
 train_size = int(0.8*len(dataset))
 train_data = dataset.iloc[:train_size, :]
 train_data.to_parquet("output/trainData_v1/2018/train_data.parquet")
-train = train_data.drop(columns = ["wgt_nominal", "dataset", "label"]).values
+#train_data = train_data.loc[train_data["dimuon_mass"] > 200., :].copy()
+train = train_data.drop(columns = ["wgt_nominal", "dataset", "label", "dimuon_mass"]).values
 train_labels = train_data["label"].values
 train_wgt = train_data["wgt_nominal"].values
 val_data = dataset.iloc[train_size:, :]
 val_data.to_parquet("output/trainData_v1/2018/val_data.parquet")
-val = val_data.drop(columns = ["wgt_nominal", "dataset", "label"]).values
+#val_data = val_data.loc[val_data["dimuon_mass"] > 200., :].copy()
+val = val_data.drop(columns = ["wgt_nominal", "dataset", "label", "dimuon_mass"]).values
 val_labels = val_data["label"].values
 val_wgt = val_data["wgt_nominal"].values
 
@@ -117,38 +120,38 @@ for epoch in range(num_epochs):
     tot_wgt = 0
     val_mean_loss = 0
     val_tot_wgt = 0
-    for i in range(train_size):  
+    for i in range(int(train_size/batch_size)):  
         # Move tensors to the configured device
-        data = torch.from_numpy(train[i]).to(device)
-        label = torch.from_numpy(np.array([train_labels[i]])).to(device)
+        data = torch.from_numpy(train[i*batch_size: (i+1)*batch_size]).to(device)
+        label = torch.from_numpy(train_labels[i*batch_size: (i+1)*batch_size].reshape((batch_size,1))).to(device)
         #w = torch.from_numpy(train_wgt[i]).to(device)
-        w = train_wgt[i]
+        w = torch.from_numpy(train_wgt[i*batch_size: (i+1)*batch_size]).to(device)
         # Forward pass
         outputs = model(data.double()) 
         loss = criterion(outputs, label.double())
         weight_loss = loss*w
         # Backward and optimize
         optimizer.zero_grad()
-        weight_loss.backward()
+        weight_loss.mean().backward()
         optimizer.step()
-        mean_loss += weight_loss.item()
-        tot_wgt += w
+        mean_loss += weight_loss.mean().item()*batch_size
+        tot_wgt += sum(w.cpu().detach().numpy())
         if i%4 == 0:
             j = int(i/4)
-            val_data = torch.from_numpy(val[j]).to(device)
-            val_label = torch.from_numpy(np.array([val_labels[j]])).to(device)
-            val_w = val_wgt[j]
+            val_data = torch.from_numpy(val[j*batch_size: (j+1)*batch_size]).to(device)
+            val_label = torch.from_numpy(val_labels[j*batch_size: (j+1)*batch_size].reshape(val_labels[j*batch_size: (j+1)*batch_size].shape[0],1)).to(device)
+            val_w = torch.from_numpy(val_wgt[j*batch_size: (j+1)*batch_size]).to(device)
             val_outputs = model(val_data.double())
             #if abs(val_outputs)>1:
             #    print(val_data)
             #    print(val_output)
             val_loss = criterion(val_outputs, val_label.double())
-            val_mean_loss += val_loss.item()*val_w
-            val_tot_wgt += val_w
-
-        if (i+1) % 10000 == 0:
+            val_mean_loss += (val_loss*val_w).mean().item()*batch_size
+            val_tot_wgt += sum(val_w.cpu().detach().numpy())
+            
+        if (i+1) % 1000 == 0:
             print ('Epoch [{}/{}], Step [{}/{}], Train Loss: {:.4f}, Val Loss: {:.4f}' 
-                   .format(epoch+1, num_epochs, i+1, total_step, mean_loss/tot_wgt, val_mean_loss/val_tot_wgt))
+                   .format(epoch+1, num_epochs, i+1, int(total_step/batch_size), mean_loss/tot_wgt, val_mean_loss/val_tot_wgt))
             mean_loss=0
             tot_wgt=0
             val_mean_loss=0
